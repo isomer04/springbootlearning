206b2c6f1774aaf6c74b79de956da5c8df544418
isomer04 <43922158+isomer04@users.noreply.github.com>
2023-10-17 22:57:07 -0400

    Added PUT/UPDATE functionality

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -53,5 +55,33 @@ public class StudentService {
         }
 
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException(
+                                "student with id " + studentId + " does not exist"
+
+                        )
+                );
+
+        if (name != null && name.length() > 0 &&
+                !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+
+        }
+
+        if (email != null && email.length() > 0 &&
+                !Objects.equals(student.getEmail(), email)) {
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("email is taken");
+            }
+
+            student.setEmail(email);
+        }
+
+
     }
 }
